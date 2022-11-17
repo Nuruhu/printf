ef72f686b69ee940c1163523aadbacd87dbe7c96
Nuruhu <nuruhussen4040@gmail.com>
2022-11-17 07:08:37 -0800

    update

--- main.h
+++ main.h
@@ -40,6 +40,7 @@ int _strlenc(const char *s);
 int printf_37(void);
 int printf_char(va_list val);
 int printf_string(va_list val);
+int get_flags(const char *format, int *i);
 int _putchar(char c);
 int _printf(const char *format, ...);
 
