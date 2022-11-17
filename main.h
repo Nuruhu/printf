81c73561e757daabe1fa5e678d69893e37463c43
Nuruhu <nuruhussen4040@gmail.com>
2022-11-17 09:03:15 -0800

    update

--- main.h
+++ main.h
@@ -6,6 +6,12 @@
 #include <limits.h>
 #include <unistd.h>
 
+#define F_MINUS 1
+#define F_PLUS 2
+#define F_ZERO 4
+#define F_HASH 8
+#define F_SPACE 16
+
 /**
  * struct format - match the conversion specifiers for printf
  * @id: type char pointer of the specifier i.e (l, h) for (d, i, u, o, x, X)
@@ -43,5 +49,18 @@ int printf_string(va_list val);
 int get_flags(const char *format, int *i);
 int _putchar(char c);
 int _printf(const char *format, ...);
+/* width handler */
+/*int handle_write_char(char c, char buffer[],
+			int flags, int width, int precision, int size);
+int write_number(int is_positive, int ind, char buffer[],
+			int flags, int width, int precision, int size);
+int write_num(int ind, char bff[], int flags, int width, int precision,
+			int length, char padd, char extra_c);
+int write_pointer(char buffer[], int ind, int length,
+			int width, int flags, char padd, char extra_c, int padd_start);
 
+int write_unsgnd(int is_negative, int ind,
+		char buffer[],
+			int flags, int width, int precision, int size);
+*/
 #endif
